acc3158cabe166d8df9cdb022d8e6db652fec45d
devarashs <southernwavesofmadness@protonmail.com>
2024-02-03 20:05:24 +0330

    Add NoAuth to handle routes that should be visible to unauthenticated users

---
+++ src/components/Authentication/NoAuth.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserInfo } from '@/lib/redux/User/UserSlice';
+
+interface AuthProtectedProps {
+  children: React.ReactNode;
+}
+
+const AuthProtected: React.FC<AuthProtectedProps> = ({ children }) => {
+  const navigate = useNavigate();
+  const userInfo = useSelector(selectUserInfo);
+
+  if (userInfo) navigate('/');
+
+  return <>{children}</>;
+};
+
+export default AuthProtected;
