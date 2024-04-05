19f787b840aadef6e8046991d9df41cd6220874c
devarashs <southernwavesofmadness@protonmail.com>
2024-04-05 16:44:56 +0330

    Add ticket search functionality to ListTicketsPage

--- src/pages/ticket/ListTickets.page.tsx
+++ src/pages/ticket/ListTickets.page.tsx
@@ -1,8 +1,28 @@
 import { Button, Container, Group } from '@mantine/core';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
+import { getData } from '@/lib/utils/getData';
 
 export default function ListTicketsPage() {
+  const [pageNumber, setPageNumber] = React.useState(1);
+  const [limit, setLimit] = React.useState(10);
+  const [timeCreatedSearchInputGTE, setTimeCreatedSearchInputGTE] = React.useState<Date | null>(
+    null
+  );
+  const [timeCreatedSearchInputLTE, setTimeCreatedSearchInputLTE] = React.useState<Date | null>(
+    null
+  );
+  const [desc, setDesc] = React.useState(false);
+  const Tickets = useQuery('search-tickets', () =>
+    getData(
+      `/ticket?pageNumber=${pageNumber}&limit=${limit}&timeCreatedGTE=${
+        timeCreatedSearchInputGTE ? timeCreatedSearchInputGTE?.toISOString() : ''
+      }&timeCreatedLTE=${
+        timeCreatedSearchInputLTE ? timeCreatedSearchInputLTE?.toISOString() : ''
+      }&desc=${desc}`
+    )
+  );
   return (
     <Container size="xl">
       <Group>
